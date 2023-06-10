c8496a12a11ed97e960d5a474f5b24ac185163cf
Janvarev Vladislav <janvarev@gmail.com>
2023-06-10 15:12:41 +0300

    обновлен каталог плагинов fuzzy_thefuzz и fuzzy_ai_sentence

--- runva_cmdline.py
+++ runva_cmdline.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     # почему бы сразу не отладить какую-то команду?
     time.sleep(0.5) # небольшой таймаут
-    cmd = "привет"
+    cmd = "мультик простоквашино"
     try:
         cmd_core.execute_next(cmd,cmd_core.context)
     except:
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         traceback.print_exc()
 
 
-    exit(0) # если нужно - закомментируйте и можно будет работать с командной строкой
+    #exit(0) # если нужно - закомментируйте и можно будет работать с командной строкой
 
     print("Enter command (user text like 'привет') or 'exit'")
     while True:
