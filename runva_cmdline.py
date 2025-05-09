2dbaf41a9082992089b8a599eb20840257a89c07
Grayen <grayen.mail@gmail.com>
2025-05-09 17:40:50 +0300

    added logging and try/except on input

--- runva_cmdline.py
+++ runva_cmdline.py
@@ -2,6 +2,9 @@ import traceback
 
 from vacore import VACore
 import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 # ------------------- main loop ------------------
 if __name__ == "__main__":
@@ -14,19 +17,23 @@ if __name__ == "__main__":
     cmd = "привет"
     try:
         cmd_core.execute_next(cmd,cmd_core.context)
-    except:
+    except Exception as e:
         if cmd == "привет":
             print("Ошибка при запуске команды 'привет'. Скорее всего, проблема с TTS.")
-        import traceback
-        traceback.print_exc()
+        # import traceback
+        # traceback.print_exc()
+        logger.exception(e)
 
 
     exit(0) # если нужно - закомментируйте и можно будет работать с командной строкой
 
     print("Enter command (user text like 'привет') or 'exit'")
     while True:
-        cmd = input("> ")
-        if cmd == "exit":
-            break
+        try:
+            cmd = input("> ")
+            if cmd == "exit":
+                break
+        except Exception as e:
+            logger.exception(e)
 
         cmd_core.execute_next(cmd,cmd_core.context)
